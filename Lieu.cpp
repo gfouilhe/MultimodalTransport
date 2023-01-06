bf70f77dea20403fd8090f4825e2818fbb92191f
gfouilhe <g.g.a.fouilhe@gmail.com>
2023-01-06 15:10:11 +0100

    présentation

--- Lieu.cpp
+++ Lieu.cpp
@@ -4,68 +4,54 @@
 
 using namespace std;
 
-void Lieu::setX(double position_x)
-{
+void Lieu::setX(double position_x){
     this->position_x = position_x;
 }
 
-void Lieu::setY(double position_y)
-{
+void Lieu::setY(double position_y){
     this->position_y = position_y;
 }
 
-void Lieu::setAltitude(double Altitude)
-{
+void Lieu::setAltitude(double Altitude){
     this->altitude = Altitude;
 }
 
-void Lieu::setVoiture(bool voiture)
-{
+void Lieu::setVoiture(bool voiture){
     this->voiture = voiture;
 }
 
-void Lieu::setVelo(bool velo)
-{
+void Lieu::setVelo(bool velo){
     this->velo = velo;
 }
 
-void Lieu::setMetro(bool metro)
-{
+void Lieu::setMetro(bool metro){
     this->metro = metro;
 }
 
-
-void Lieu::setPieds(bool pieds)
-{
+void Lieu::setPieds(bool pieds){
     this->pieds = pieds;
 }
 
-double Lieu::getX() const
-{
+double Lieu::getX() const{
     return this->position_x;
 } 
 
-double Lieu::getY() const
-{
+double Lieu::getY() const{
     return this->position_y;
 } 
 
-double Lieu::distance(const Lieu &LieuBis) const // à réflechir pour le calcul de la distance par métro
-{
+double Lieu::distance(const Lieu &LieuBis) const {// à réflechir pour le calcul de la distance par métro
     double dx = this->position_x - LieuBis.position_x;
     double dy = this->position_y - LieuBis.position_x;
     return sqrt(dx*dx + dy*dy); 
 }
 
-
-void Lieu::saisir()
-{
+void Lieu::saisir(){
     cout << "Tapez l'abscisse : ";  cin >> this->position_x; cout << endl;
     cout << "Tapez l'ordonnée : ";  cin >> this->position_y; cout << endl;
 } 
 
-void Lieu::afficher() const
-{
+void Lieu::afficher() const{
     cout << "L'abscisse vaut " << this->position_x << endl; 
     cout << "L'ordonnée vaut " << this->position_y << endl;
 }
